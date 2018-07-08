630b86390abd0a575ad8e162c4d0818b3dba22e7
Jeremy Mullis <jeremy.mullis@gmail.com>
2018-07-08 11:44:47 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = function(options) {
+  return function({ addComponents }) {
+    addComponents({
+      ".embed-responsive": {
+        position: "relative",
+        display: "block",
+        height: "0",
+        padding: "0",
+        overflow: "hidden",
+        ".embed-responsive-item,&>iframe,&>embed,&>object,&>video": {
+          position: "absolute",
+          top: "0",
+          left: "0",
+          bottom: "0",
+          height: "100%",
+          width: "100%",
+          border: "0"
+        }
+      }
+    });
+  };
+};
